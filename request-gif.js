3bd430f272277427350affda2e13ac49c0f1a9f4
Michael Harris <avant.f.music@gmail.com>
2017-04-03 16:35:37 -0400

    added bootstrap

--- request-gif.js
+++ request-gif.js
@@ -1,47 +1,60 @@
-
-
 $(document).ready(function() {
-    // register our function as the "callback" to be triggered by the form's submission event
-    $("#form-gif-request").submit(fetchAndDisplayGif); // in other words, when the form is submitted, fetchAndDisplayGif() will be executed
+    $("#form-gif-request").submit(fetchAndDisplayGif);
 });
+
 
 function fetchAndDisplayGif(event) {
 
     event.preventDefault();
 
-    var searchQuery = $("#form-gif-request input[name=tag]").val();
     var captcha = $("#form-gif-request input[name=captcha]").val();
+    var searchQuery = $("#form-gif-request input[name=tag]").val();
+    console.log(searchQuery);
 
 
 
 
     var params = {
         api_key: "dc6zaTOxFJmzC",
-        tag : "jackson 5" + searchQuery
+        tag :"jackson 5" + searchQuery,
     };
 
-    $.ajax({
-        url: "http://api.giphy.com/v1/gifs/random",
-        data: params,
-        success: function(response) {
+        if(captcha === "5"){
 
-            $("#gif").attr("src", response.data.image_url)
-            .attr("hidden", false);
-            $("feedback").attr("hidden", true);
+            $.ajax({
+                url: "http://api.giphy.com/v1/gifs/random",
+                data: params,
 
-        },
-        error: function() {
-            $("#feedback").text("Sorry, could not load GIF. Try again!");
-            setGifLoadedStatus(false);
+                beforeSend: function(){
+                    $("#loading").attr("hidden", false)
+                        .text("loading.....")
+                },
+
+                success: function(response) {
+
+                    $("#gif").attr("src", response.data.image_url)
+                    .attr("hidden", false);
+                    $("feedback").attr("hidden", true);
+                    $("#fail").attr("hidden", true);
+                    $("#loading").attr("hidden", true)
+
+                },
+                error: function() {
+                    $("#feedback").text("Sorry, could not load GIF. Try again!");
+                    setGifLoadedStatus(false);
+                }
+            });
+
         }
-    });
+        else{
+            $("#fail").text("No gif 4 U")
+                .attr("hidden", false);
+        }
 
-    // TODO
-    // give the user a "Loading..." message while they wait
 
-}
+    }
 
-function setGifLoadedStatus(isCurrentlyLoaded) {
-    $("#gif").attr("hidden", !isCurrentlyLoaded);
-    $("#feedback").attr("hidden", isCurrentlyLoaded);
-}
+    function setGifLoadedStatus(isCurrentlyLoaded) {
+        $("#gif").attr("hidden", !isCurrentlyLoaded);
+        $("#feedback").attr("hidden", isCurrentlyLoaded);
+    }
